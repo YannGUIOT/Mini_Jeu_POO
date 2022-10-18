c44bb1ae8a5aa8d022babfb52217926aa243da25
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-18 18:47:44 +0200

    play game

--- app_3.rb
+++ app_3.rb
@@ -7,14 +7,7 @@ require_relative 'lib/player'
 
 my_game = Game.new("PLAYER")
 
-opponent1 = HumanPlayer.new("DYLAN LE BADASS")
-opponent2 = HumanPlayer.new("GILLIAN LE TYRAN")
 
 # binding.pry
 
 
-
-
-player = init_game
-players_statut(player,opponent1,opponent2)
-fight(player,opponent1,opponent2)
